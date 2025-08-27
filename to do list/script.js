b522dd1ab5614a19f0d999a89d896c6485ddb421
Anuj Singh Bisht <anujbisht.dev@gmail.com>
2025-08-27 23:57:07 +0530

    script.js

--- to do list/script.js
+++ to do list/script.js
@@ -10,6 +10,50 @@ let todaysGoal = document.getElementById("todays_goal_textarea");
 let googleTextarea = document.getElementById("type_something");
 let searchButton = document.getElementById("search");
 
+
+const mainWorking = function (key, values) {
+    const upper_div = document.createElement('div');
+    div_storage.appendChild(upper_div);
+
+    const create_div = document.createElement("div");
+    create_div.classList.add('create_div');
+    create_div.innerHTML = values;
+    upper_div.appendChild(create_div);
+
+    const tick_image = document.createElement('img');
+    tick_image.src = './image/tick.png';
+    tick_image.setAttribute('id', 'div_tick_btn');
+
+    const remove_image = document.createElement('img');
+    remove_image.src = './image/remove.png';
+    remove_image.setAttribute('id', 'div_remove_btn');
+
+    const delete_image = document.createElement('img');
+    delete_image.src = './image/delete.png';
+    delete_image.setAttribute('id', 'div_delete_btn');
+
+    upper_div.appendChild(tick_image);
+    upper_div.appendChild(remove_image);
+    upper_div.appendChild(delete_image);
+
+    tick_image.addEventListener('click', () => {
+        create_div.style.background = 'rgba(0, 255, 0, 0.35)';
+    })
+
+    remove_image.addEventListener('click', () => {
+        create_div.style.background = 'rgba(255, 0, 0, 0.3)';
+    })
+
+    delete_image.addEventListener('click', () => {
+        upper_div.remove();
+        localStorage.removeItem(key);
+    })
+    delete_image.addEventListener('click', () => {
+        upper_div.remove();
+        localStorage.removeItem(key);
+    });
+}
+
 // window loading
 window.onload = function () {
     if (localStorage.length === 0) {
@@ -20,45 +64,10 @@ window.onload = function () {
             let key = localStorage.key(i);
             let values = localStorage.getItem(key);
             if (key === "goal") {
-                continue
+                continue;
             }
 
-            const upper_div = document.createElement('div');
-            div_storage.appendChild(upper_div);
-
-            const create_div = document.createElement("div");
-            create_div.classList.add('create_div');
-            create_div.innerHTML = values;
-            upper_div.appendChild(create_div);
-
-            const tick_image = document.createElement('img');
-            tick_image.src = './image/tick.png';
-            tick_image.setAttribute('id', 'div_tick_btn');
-
-            const remove_image = document.createElement('img');
-            remove_image.src = './image/remove.png';
-            remove_image.setAttribute('id', 'div_remove_btn');
-
-            const delete_image = document.createElement('img');
-            delete_image.src = './image/delete.png';
-            delete_image.setAttribute('id', 'div_delete_btn');
-
-            upper_div.appendChild(tick_image);
-            upper_div.appendChild(remove_image);
-            upper_div.appendChild(delete_image);
-
-            tick_image.addEventListener('click', () => {
-                create_div.style.background = 'rgba(0, 255, 0, 0.35)';
-            })
-
-            remove_image.addEventListener('click', () => {
-                create_div.style.background = 'rgba(255, 0, 0, 0.3)';
-            })
-
-            delete_image.addEventListener('click', () => {
-                upper_div.remove();
-                localStorage.removeItem(key);
-            })
+            mainWorking(key, values);
         }
     }
     // trim() used for ignoring empty string to save
@@ -68,8 +77,7 @@ window.onload = function () {
     todaysGoal.value = localStorage.getItem("goal");
 }
 
-
-todaysGoal.addEventListener("mouseout", () => {
+todaysGoal.addEventListener("change", () => {
     if (todaysGoal.value.trim()) {
         localStorage.setItem("goal", todaysGoal.value);
     }
@@ -87,7 +95,7 @@ setInterval(() => {
     time.innerText = new Date().toString().split(" ")[4]
 }, 1000)
 setInterval(() => {
-    date.innerText = new Date().toString().slice(0, 15)
+    date.innerText = new Date().toString().slice(0, 15);
 })
 
 // search on google window.open() used
@@ -112,47 +120,13 @@ upper_tick_btn.addEventListener("click", () => {
         alert('Enter the text');
     }
     else {
-        const upper_div = document.createElement('div');
-        div_storage.appendChild(upper_div);
-
-        const create_div = document.createElement("div");
-        create_div.classList.add('create_div');
-        create_div.textContent = input_box.value;
-        upper_div.appendChild(create_div);
-        input_box.value = '';
+        let key = Date.now().toString();
+        let values = input_box.value.trim();
+        mainWorking(key, values);
 
         // set Item into local storage
-        let key = upper_div.childElementCount;
-        let value = create_div.innerText;
-        localStorage.setItem(localStorage.length, value);
+        localStorage.setItem(key, values);
 
-
-        const tick_image = document.createElement('img');
-        tick_image.src = './image/tick.png';
-        tick_image.setAttribute('id', 'div_tick_btn');
-
-        const remove_image = document.createElement('img');
-        remove_image.src = './image/remove.png';
-        remove_image.setAttribute('id', 'div_remove_btn');
-
-        const delete_image = document.createElement('img');
-        delete_image.src = './image/delete.png';
-        delete_image.setAttribute('id', 'div_delete_btn');
-
-        upper_div.appendChild(tick_image);
-        upper_div.appendChild(remove_image);
-        upper_div.appendChild(delete_image);
-
-        tick_image.addEventListener('click', () => {
-            create_div.style.background = 'rgba(0, 255, 0, 0.35)';
-        })
-
-        remove_image.addEventListener('click', () => {
-            create_div.style.background = 'rgba(255, 0, 0, 0.3)';
-        })
-
-        delete_image.addEventListener('click', () => {
-            upper_div.remove('div');
-        })
+        input_box.value = '';
     }
 });
